9eda641ae4f4dcef1a26a704ddf16e1b5d885526
kconners-sec <kconners@securustechnologies.com>
2023-09-15 16:07:48 -0500

    Initial Commit

---
+++ DieCastCollection/DieCastCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DieCastCollectionApp.swift
+//  DieCastCollection
+//
+//  Created by Kaylen Conners on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DieCastCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
